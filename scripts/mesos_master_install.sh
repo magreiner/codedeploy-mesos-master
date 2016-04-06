51482c29caed7dd1af66164d65fecc1038698c65
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-04-06 19:21:28 +0200

    upload test template

--- scripts/mesos_master_install.sh
+++ scripts/mesos_master_install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
+# Add mesos repository
 apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" > /etc/apt/sources.list.d/mesosphere.list
+
+# Add java repository
 add-apt-repository -y ppa:webupd8team/java
 su -c 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
+
+# install requirements
 apt-get --yes update
 apt-get --yes install oracle-java8-installer
 apt-get --yes install mesos marathon chronos curl
+
 mkdir -p /var/lib/mesos
 chown ubuntu:ubuntu /var/lib/mesos
 
+# start services
 service mesos-master start
 service marathon start
 
@@ -29,7 +37,7 @@ source bin/env-setup-dev
 dcos config set core.mesos_master_url http://localhost:5050
 
 cd /tmp
-curl -X POST http://localhost:8080/v2/apps -d @basic.json -H "Content-type: application/json"
+curl -X PUT http://localhost:8080/v2/groups -d @basic.json -H "Content-type: application/json"
 
 
 #MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
