676bf326f03aac35669f7f84c0cebffa13fede43
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-04-06 22:31:32 +0200

    used alternative config

--- scripts/mesos_master_install.sh
+++ scripts/mesos_master_install.sh
@@ -1,44 +1,33 @@
 #!/bin/bash
 
+LOCAL_IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+
 # Add mesos repository
 apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" > /etc/apt/sources.list.d/mesosphere.list
 
-# Add java repository
-add-apt-repository -y ppa:webupd8team/java
-su -c 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections'
-
 # install requirements
 apt-get --yes update
-apt-get --yes install oracle-java8-installer
-apt-get --yes install mesos marathon chronos curl
+apt-get --yes install mesos marathon curl
 
-mkdir -p /var/lib/mesos
-chown ubuntu:ubuntu /var/lib/mesos
+
+sudo service mesos-slave stop
+echo manual | tee /etc/init/mesos-slave.override
+echo $LOCAL_IP_ADDRESS | tee /etc/mesos-master/ip
+echo zk://$LOCAL_IP_ADDRESS:2181/mesos | tee /etc/mesos/zk
+echo TestCluster | tee /etc/mesos-master/cluster
+echo $LOCAL_IP_ADDRESS | sudo tee /etc/mesos-master/hostname
+echo 1 | tee /etc/zookeeper/conf/myid
 
 # start services
-service mesos-master start
-service marathon start
+service zookeeper restart
+service mesos-master restart
+service marathon restart
 
-#
-# Install dcos
-#
-apt-get --yes install python-pip python-virtualenv python-dev
-cd /tmp
-git clone https://github.com/mesosphere/dcos-cli.git
-cd dcos-cli
-make env
-make packages
-cd cli
-make env
-source bin/env-setup-dev
-dcos config set core.mesos_master_url http://localhost:5050
-
-cd /tmp
-curl -X PUT http://localhost:8080/v2/groups -d @basic.json -H "Content-type: application/json"
+# cd /tmp
+# curl -X PUT http://localhost:8080/v2/groups -d @basic.json -H "Content-type: application/json"
 
 
-#MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-#screen -dmS mesos-master bash -c  "/usr/sbin/mesos-master --ip=$MY_IP --work_dir=/var/lib/mesos"
+#screen -dmS mesos-master bash -c  "/usr/sbin/mesos-master --ip=$LOCAL_IP_ADDRESS --work_dir=/var/lib/mesos"
