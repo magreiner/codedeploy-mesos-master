0d9b9ee9da1aa7c6b2358f9102db44f5f4d9858c
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-04-19 00:45:29 +0200

    added vault in dev mode

--- scripts/cluster_settings.sh
+++ scripts/cluster_settings.sh
@@ -6,8 +6,7 @@ AZ="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zon
 REGION="${AZ::-1}"
 
 LOCAL_IP_ADDRESS="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)"
-ACCESS_SERVER_IP="$(aws ec2 describe-instances --region $REGION --filters "Name=tag:Name,Values=$ACCESS_INSTANCE_TAGNAME" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp}' | grep "\." | cut -f4 -d'"')
-"
+ACCESS_SERVER_IP="$(aws ec2 describe-instances --region $REGION --filters "Name=tag:Name,Values=$ACCESS_INSTANCE_TAGNAME" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp}' | grep "\." | cut -f4 -d'"')"
 
 # install requirements
 apt-get install pwgen
@@ -16,17 +15,16 @@ apt-get install pwgen
 sleep 20
 
 # Mysql Settings
-# consulate kv ls
-# consulate kv mkdir mysql
-# consulate kv set mysql/MYSQL_ROOT_PASS "$(pwgen)"
-# consulate kv set mysql/MYSQL_SEAFILE_USER "seafile"
-# consulate kv set mysql/MYSQL_SEAFILE_PASS "$(pwgen)"
-#
-# # Seafile Settings
-# consulate kv mkdir seafile
-# consulate kv set seafile/SEAFILE_ADMIN_EMAIL "adm@adm.de"
-# consulate kv set seafile/SEAFILE_ADMIN_PASS "$(pwgen)"
-# consulate kv set seafile/SEAFILE_SERVER_IP "$ACCESS_SERVER_IP"
+export VAULT_ADDR="http://$LOCAL_IP_ADDRESS:8201"
+export VAULT_TOKEN="$(cat /root/vault.keys | grep "Root Token: " | cut -d" " -f3)"
+vault write secret/mysql/MYSQL_ROOT_PASS value="$(pwgen)"
+vault write secret/mysql/MYSQL_SEAFILE_USER value="$seafile"
+vault write secret/mysql/MYSQL_SEAFILE_PASS value="$(pwgen)"
+
+# Seafile Settings
+vault write secret/seafile/SEAFILE_ADMIN_EMAIL value="adm@adm.de"
+vault write secret/seafile/SEAFILE_ADMIN_PASS value="$(pwgen)"
+vault write secret/seafile/SEAFILE_SERVER_IP value="$ACCESS_SERVER_IP"
 
 # Start Services
 curl -X PUT http://localhost:8080/v2/apps -d @/tmp/basic.json -H "Content-type: application/json" &> /tmp/basic.log
--- scripts/vault.sh
+++ scripts/vault.sh
@@ -7,3 +7,29 @@ apt-get install -yq unzip
 unzip -o /tmp/vault_*_linux_amd64.zip -d /usr/bin/
 rm /tmp/vault_*_linux_amd64.zip
 chmod +x /usr/bin/vault
+
+mkdir /etc/vault
+cat > /etc/vault/config.json <<EOF
+backend "inmem" {
+  address = "127.0.0.1:8500"
+  path = "vault"
+}
+
+listener "tcp" {
+ address = "0.0.0.0:8201"
+ tls_disable = 1
+}
+EOF
+
+
+# screen -dmS mesos-agent bash -c  "/usr/bin/vault server -config=/etc/vault/config.json"
+screen -dmS mesos-agent bash -c  "/usr/bin/vault server -config=/etc/vault/config.json -dev"
+# export VAULT_ADDR='http://127.0.0.1:8200'
+# vault init &>/root/vault.keys
+#
+# chmod +x unseal.expect
+# vault unseal $(cat /root/vault.keys | grep "Key 1" | cut -d" " -f3)
+# vault unseal $(cat /root/vault.keys | grep "Key 2" | cut -d" " -f3)
+# vault unseal $(cat /root/vault.keys | grep "Key 3" | cut -d" " -f3)
+#
+echo "$(cat /root/vault.keys | grep "Root Token: " | cut -d" " -f3)" > .vtoken
